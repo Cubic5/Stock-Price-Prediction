1ce9e7f72a58af186162eda8be57ddce4212439e
Cubic5 <78907127+Cubic5@users.noreply.github.com>
2024-10-22 10:50:58 +0200

    Update untitled5.py

--- untitled5.py
+++ untitled5.py
@@ -13,7 +13,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 #import matplotlib.pyplot as plt
-import seaborn as sns
+#import seaborn as sns
 import yfinance as yf
 import joblib, os
 
