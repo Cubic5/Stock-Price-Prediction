675343152bb38907115e81993320b689f10cc64c
Cubic5 <78907127+Cubic5@users.noreply.github.com>
2024-10-22 13:34:13 +0200

    Update untitled5.py

--- untitled5.py
+++ untitled5.py
@@ -12,10 +12,10 @@ Original file is located at
 import streamlit as st
 import pandas as pd
 import numpy as np
-#import matplotlib.pyplot as plt
-#import seaborn as sns
-#import yfinance as yf
-#import joblib, os
+import matplotlib.pyplot as plt
+import seaborn as sns
+import yfinance as yf
+import joblib, os
 
 saved_model = joblib.load('arima_model.pkl')
 
