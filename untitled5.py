6d4426fc5afe53b87baa956de1d34335e5133eab
Cubic5 <78907127+Cubic5@users.noreply.github.com>
2024-10-29 10:48:30 +0200

    Update untitled5.py

--- untitled5.py
+++ untitled5.py
@@ -35,8 +35,7 @@ def fetch_stock_data(ticker, start_date, end_date):
 
 # Define a function to apply Box-Cox transformation
 def apply_boxcox_transformation(data):
-    data = data.reset_index('Date')
-    data = pd.to_datetime(data['Date'])
+    data.index = pd.to_datetime(data.index)
     df_arima = data[['Close']].copy() # Extract the 'Close' price column
     df_arima['Close'], _ = boxcox(df_arima['Close'])
     return df_arima
