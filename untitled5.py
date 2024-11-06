d1454e69a869d537f826b850c270185b0be6d8cd
Cubic5 <78907127+Cubic5@users.noreply.github.com>
2024-11-06 13:48:05 +0200

    Update untitled5.py

--- untitled5.py
+++ untitled5.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import boxcox
+from scipy.special import inv_boxcox
 import seaborn as sns
 import yfinance as yf
 import joblib, os
