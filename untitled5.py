12af48220334b0152f6dd7ae25aaef8ea2a99871
Cubic5 <78907127+Cubic5@users.noreply.github.com>
2024-11-12 09:24:54 +0200

    Update untitled5.py

--- untitled5.py
+++ untitled5.py
@@ -40,7 +40,7 @@ def apply_boxcox_transformation(data):
     return df_arima
 
 def train_auto_arima_model(data):
-    model = auto_arima(
+    model = pm.auto_arima(
         data['Close'],
         start_p=1, start_q=1,
         max_p=5, max_q=5,
